0339011c01abcae73fca67e4b31d6856af719e85
Sarah Paz <sarah.paz@gmail.com>
2020-01-30 23:22:45 -0500

    create js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const calculator = document.querySelector(".calculator-container");
+const keys = calculator.querySelector(".buttons-container");
+
+keys.addEventListener("click", e => {
+  if (e.target.matches("div")) {
+  }
+});
