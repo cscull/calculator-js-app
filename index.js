86c292f44574f75a08ce47c057ab05a96525fbf2
Sarah Paz <sarah.paz@gmail.com>
2020-02-22 22:19:59 -0500

    add logic to determine button clicks

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const calculator = document.querySelector(".calculator-container");
-const keys = calculator.querySelector(".buttons-container");
+const buttons = calculator.querySelector(".buttons-container");
 
-keys.addEventListener("click", e => {
+buttons.addEventListener("click", e => {
   if (e.target.matches("div")) {
+    const button = e.target;
+    const action = button.dataset.action;
+
+    if (!action) {
+      console.log("number key!");
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key!");
+    }
   }
 });
