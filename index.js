264fb6ec1f53ae78f0c8d1a836e7ca499ff74be9
Sarah Paz <sarah.paz@gmail.com>
2020-02-23 00:14:45 -0500

    add conditionals to display button click

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const calculator = document.querySelector(".calculator-container");
 const buttons = calculator.querySelector(".buttons-container");
+const display = calculator.querySelector(".display");
 
 buttons.addEventListener("click", e => {
   if (e.target.matches("div")) {
     const button = e.target;
     const action = button.dataset.action;
+    const buttonContent = button.textContent;
+    const displayedNumber = display.textContent;
 
     if (!action) {
-      console.log("number key!");
+      if (displayedNumber == 0) {
+        display.textContent = buttonContent;
+      } else {
+        display.textContent = displayedNumber + buttonContent;
+      }
     }
     if (
       action === "add" ||
@@ -16,6 +23,17 @@ buttons.addEventListener("click", e => {
       action === "divide"
     ) {
       console.log("operator key!");
+    }
+    if (action === "decimal") {
+      console.log("decimal key!", display.textContent);
+      display.textContent = displayedNumber + ".";
+    }
+    if (action === "clear") {
+      console.log("clear key!");
+      display.textContent = "0";
+    }
+    if (action === "equal") {
+      console.log("equal key!");
     }
   }
 });
