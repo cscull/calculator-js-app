b066a99b9bf39b5dbf035ebf0f048ad8d44fac22
Sarah Paz <sarah.paz@gmail.com>
2020-03-03 08:12:20 -0500

    add variables for buttons

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("data-number");
+const dataOperations = document.querySelectorAll("data-operation");
+const displayPrevious = document.querySelector("data-previous");
+const displayCurrent = document.querySelector("data-current");
+const allClearButton = document.querySelector("data-all-clear");
+const deleteButton = document.querySelector("data-delete");
+const equalsButton = document.querySelector("data-equals");
